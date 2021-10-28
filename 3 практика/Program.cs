a43a505055e3d395998a56871d90b80f28481a78
st23012004 <tanyazhideeva2004@gmail.com>
2021-10-28 12:38:02 +0300

    Добавьте файлы проекта.

---
+++ 3 практика/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_практика
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
